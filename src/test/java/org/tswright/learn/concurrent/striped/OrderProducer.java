f0c334283a916644c84ccbef615514cd9d7c302e
Scott Wright <swright@neovest.com>
2017-03-02 16:28:28 -0700

    Added logging to the test.

--- src/test/java/org/tswright/learn/concurrent/striped/OrderProducer.java
+++ src/test/java/org/tswright/learn/concurrent/striped/OrderProducer.java
@@ -1,7 +1,6 @@
 package org.tswright.learn.concurrent.striped;
 
 import static java.lang.String.format;
-import static java.lang.System.out;
 import static java.util.Objects.requireNonNull;
 import static java.util.concurrent.ThreadLocalRandom.current;
 
@@ -9,6 +8,9 @@ import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.CyclicBarrier;
 import java.util.stream.IntStream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author tswright
@@ -19,6 +21,7 @@ class OrderProducer extends Thread
 	private final CountDownLatch finishLine;
 	private final StripedThreadPool<String> executor;
 	private final String orderId;
+	private final Logger logger;
 
 	OrderProducer( final CyclicBarrier startingLine, final CountDownLatch finishLine, final StripedThreadPool<String> executor, final String orderId )
 	{
@@ -26,6 +29,7 @@ class OrderProducer extends Thread
 		this.finishLine = requireNonNull( finishLine );
 		this.executor = requireNonNull( executor );
 		this.orderId = requireNonNull( orderId );
+		logger = LoggerFactory.getLogger( getClass().getSimpleName() );
 		setName( getClass().getSimpleName() + " - " + orderId );
 	}
 
@@ -47,24 +51,24 @@ class OrderProducer extends Thread
 	private void simulateOrder()
 	{
 		sleepRandom();
-		executor.dispatch( orderId, () -> out.println( format( "%s --> orderId=%s, status=Sent", currentThread().getName(), orderId ) ) );
+		executor.dispatch( orderId, () -> logger.info( format( "%s --> orderId=%s, status=Sent", currentThread().getName(), orderId ) ) );
 		sleepRandom();
-		executor.dispatch( orderId, () -> out.println( format( "%s --> orderId=%s, status=Acknowledged", currentThread().getName(), orderId ) ) );
+		executor.dispatch( orderId, () -> logger.info( format( "%s --> orderId=%s, status=Acknowledged", currentThread().getName(), orderId ) ) );
 		sleepRandom();
-		executor.dispatch( orderId, () -> out.println( format( "%s --> orderId=%s, status=Pending", currentThread().getName(), orderId ) ) );
+		executor.dispatch( orderId, () -> logger.info( format( "%s --> orderId=%s, status=Pending", currentThread().getName(), orderId ) ) );
 
 		final int partials = current().nextInt() % 8;
 		switch ( partials )
 		{
 			case 0:
 				sleepRandom();
-				executor.dispatch( orderId, () -> out.println( format( "%s --> orderId=%s, status=Executed", currentThread().getName(), orderId ) ) );
+				executor.dispatch( orderId, () -> logger.info( format( "%s --> orderId=%s, status=Executed", currentThread().getName(), orderId ) ) );
 				break;
 
 			default:
 				IntStream.range( 1, partials + 1 ).forEach( index -> fillPartial( orderId, index )  );
 				sleepRandom();
-				executor.dispatch( orderId, () -> out.println( format( "%s --> orderId=%s, status=Complete", currentThread().getName(), orderId ) ) );
+				executor.dispatch( orderId, () -> logger.info( format( "%s --> orderId=%s, status=Complete", currentThread().getName(), orderId ) ) );
 				break;
 		}
 	}
@@ -72,18 +76,18 @@ class OrderProducer extends Thread
 	private void fillPartial( final String orderId, final int index )
 	{
 		sleepRandom();
-		executor.dispatch( orderId, () -> out.println( format( "%s --> orderId=%s, status=Partial (%d)", currentThread().getName(), orderId, index ) ) );
+		executor.dispatch( orderId, () -> logger.info( format( "%s --> orderId=%s, status=Partial (%d)", currentThread().getName(), orderId, index ) ) );
 	}
 
 	private void sleepRandom()
 	{
 		try
 		{
-			Thread.sleep( current().nextInt( 0, 1000 ) );
+			Thread.sleep( current().nextInt( 0, 2000 ) );
 		}
 		catch ( final InterruptedException e )
 		{
-			e.printStackTrace();
+			logger.error( "I feel so tired; I just couldn't sleep!", e );
 		}
 	}
 }
